4c827630d69cf70a411647e7715c57df5df4a6cb
abdo6400 <abdulrahmanamr348@gmail.com>
2024-07-23 00:05:58 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,53 +4,29 @@ import 'package:flutter_portfolio/view/splash/splash_view.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: bgColor,
-        useMaterial3: true,
-        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white ,)
-            .copyWith(
-          bodyLarge: const TextStyle(color: bodyTextColor),
-          bodyMedium: const TextStyle(color: bodyTextColor),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          scaffoldBackgroundColor: bgColor,
+          useMaterial3: true,
+          textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
+              .apply(
+                bodyColor: Colors.white,
+              )
+              .copyWith(
+                bodyLarge: const TextStyle(color: bodyTextColor),
+                bodyMedium: const TextStyle(color: bodyTextColor),
+              ),
         ),
-      ),
-
-      home: SplashView()
-    );
+        home: const SplashView());
   }
 }
-
-
-/**"icons": [
-        {
-            "src": "icons/Icon-192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-        },
-        {
-            "src": "icons/Icon-512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-        },
-        {
-            "src": "icons/Icon-maskable-192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "icons/Icon-maskable-512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-        }
-    ] */
