77be4b4247fd3fc7ed4dcd7e8b881a80e1d14a41
abdo6400 <abdulrahmanamr348@gmail.com>
2024-07-23 00:35:39 +0300

    edit about info

--- lib/view/main/components/drawer/personal_info.dart
+++ lib/view/main/components/drawer/personal_info.dart
@@ -11,15 +11,20 @@ class PersonalInfo extends StatelessWidget {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: defaultPadding/2,),
-        AreaInfoText(title: 'Contact', text: '03166657602'),
-        AreaInfoText(title: 'Email', text: 'rh676838@gmail.com'),
-        AreaInfoText(title: 'LinkedIn', text: '@hamad-anwar'),
-        AreaInfoText(title: 'Github', text: '@hamad-anwar'),
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
+        AreaInfoText(title: 'Contact', text: '01069645711'),
+        AreaInfoText(title: 'Email', text: 'abdulrahman@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@abdulrhamn-amr'),
+        AreaInfoText(title: 'Github', text: '@abdulrhamn-amr'),
         SizedBox(
           height: defaultPadding,
         ),
-        Text('Skills',style: TextStyle(color: Colors.white),),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
         SizedBox(
           height: defaultPadding,
         ),
